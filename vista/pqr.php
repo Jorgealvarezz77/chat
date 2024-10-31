a0ad6870bdfffc44d8f517bf844bed66dfb79b6c
lolcristian323 <xdv5039@gmail.com>
2024-10-31 01:05:07 +0000

    el cambio de icono

--- vista/pqr.php
+++ vista/pqr.php
@@ -9,6 +9,7 @@ session_start(); // Esto debe ir al inicio del archivo
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contactos - Agroo app</title>
     <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
 
 <body>
@@ -24,7 +25,11 @@ session_start(); // Esto debe ir al inicio del archivo
                     <?php else: ?>
                         <li><a href="login.php">Iniciar sesión</a></li>
                         <li><a href="registro.php">Registrarse</a></li>
+
                     <?php endif; ?>
+                    <li class="nav-item active">
+                    <li class="nav-item active"><a class="nav-link" href="carrito.php"><i class="fa-solid fa-cart-shopping"></i>(<?php echo count($_SESSION['carrito']); ?>)</a></li>
+                </li>
                 </ul>
             </nav>
         </div>
